4a8bc635e08ffa6130b7f5562885a9afe419b17a
HayBeeCoder <haybeecodes@gmail.com>
2023-07-24 15:33:54 +0100

    second commit
    Change-Id: I9c31f52f7a11d0f8a3356a1cd25ae5ea57b447f9

---
+++ Lox/src/Lox.java
@@ -0,0 +1,9 @@
+
+public class Lox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lox/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author abass
+ *
+ */
+module Lox {
+}
